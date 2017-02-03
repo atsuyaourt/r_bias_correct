8656431e56ade3ce2711be8e5e31ef801ae5b462
Emilio Gozo <emilio.gozo@obf.ateneo.edu>
2017-02-03 14:18:30 +0800

    chore: Code reformat

--- scripts/bias_correct.R
+++ scripts/bias_correct.R
@@ -1,68 +1,68 @@
 require(dplyr)
 require(qmap)
 
-BiasCorrect <- function(df, method="Quant", wet.day=T) {
-    
-    if (method == "Li") {
-        return(.bc2(df, wet.day))
-    }
-    
-    return(.bc1(df, wet.day))
+BiasCorrect <- function(df,
+                        method = "Quant",
+                        wet.day = T) {
+  if (method == "Li") {
+    return(.bc2(df, wet.day))
+  }
+  
+  return(.bc1(df, wet.day))
 }
 
-.bc1 <- function(df, wet.day=T) {
-    fit_df <- df %>% dplyr::filter(fit)
-    
-    if (sum(!is.na(fit_df$obs))>10) {
-        qm.fit <- fitQmapQUANT(fit_df$obs, fit_df$mod, wet.day)
-    }
-    
-    if (!exists("qm.fit")) {
-        out_df <- df %>%
-            dplyr::mutate(bc=mod)
-    } else {
-        out_df <- df %>%
-            dplyr::do(data.frame(., bc=doQmap(.$mod, qm.fit)))
-    }
-    
-    return(out_df %>% dplyr::select(-fit))
+.bc1 <- function(df, wet.day = T) {
+  fit_df <- df %>% dplyr::filter(fit)
+  
+  if (sum(!is.na(fit_df$obs)) > 10) {
+    qm.fit <- fitQmapQUANT(fit_df$obs, fit_df$mod, wet.day)
+  }
+  
+  if (!exists("qm.fit")) {
+    out_df <- df %>%
+      dplyr::mutate(bc = mod)
+  } else {
+    out_df <- df %>%
+      dplyr::do(data.frame(., bc = doQmap(.$mod, qm.fit)))
+  }
+  
+  return(out_df %>% dplyr::select(-fit))
 }
 
 .bc2 <- function(df, wet.day) {
-    fit_df <- df %>% dplyr::filter(fit)
-	
-	out_df <- df %>%
-		dplyr::group_by(grp) %>%
-		dplyr::do(.bc2_helper(fit_df, ., wet.day)) %>%
-		ungroup()
-		
-	return(out_df %>% dplyr::select(-grp, -fit))
+  fit_df <- df %>% dplyr::filter(fit)
+  
+  out_df <- df %>%
+    dplyr::group_by(grp) %>%
+    dplyr::do(.bc2_helper(fit_df, ., wet.day)) %>%
+    ungroup()
+  
+  return(out_df %>% dplyr::select(-grp,-fit))
 }
 
 .bc2_helper <- function(fit_df, df, wet.day) {
-	if (unique(fit_df$grp) %in% unique(df$grp)) {
-		return(.bc1(df, wet.day))
-	}
-	
-	if (sum(!is.na(fit_df$obs))>10) {
-        qm.fit1 <- fitQmapQUANT(fit_df$obs, df$mod, wet.day)
-        qm.fit2 <- fitQmapQUANT(fit_df$mod, df$mod, wet.day)
-    }
-	
-	if (!exists("qm.fit1") | !exists("qm.fit2")) {
-        out_df <- df %>%
-            dplyr::mutate(bc=mod)
-    } else {
-        out_df <- df %>%
-            dplyr::do(data.frame(
-                .,
-                bc1=doQmap(.$mod,qm.fit1),
-                bc2=doQmap(.$mod,qm.fit2)
-            )
-            ) %>%
-            dplyr::mutate(bc=mod+bc1-bc2) %>%
-            dplyr::select(-bc1,-bc2)
-    }
-	
-	return(out_df)
+  if (unique(fit_df$grp) %in% unique(df$grp)) {
+    return(.bc1(df, wet.day))
+  }
+  
+  if (sum(!is.na(fit_df$obs)) > 10) {
+    qm.fit1 <- fitQmapQUANT(fit_df$obs, df$mod, wet.day)
+    qm.fit2 <- fitQmapQUANT(fit_df$mod, df$mod, wet.day)
+  }
+  
+  if (!exists("qm.fit1") | !exists("qm.fit2")) {
+    out_df <- df %>%
+      dplyr::mutate(bc = mod)
+  } else {
+    out_df <- df %>%
+      dplyr::do(data.frame(
+        .,
+        bc1 = doQmap(.$mod, qm.fit1),
+        bc2 = doQmap(.$mod, qm.fit2)
+      )) %>%
+      dplyr::mutate(bc = mod + bc1 - bc2) %>%
+      dplyr::select(-bc1, -bc2)
+  }
+  
+  return(out_df)
 }
--- scripts/helper.R
+++ scripts/helper.R
@@ -11,15 +11,15 @@ GetDir <- function(site, type) {
     path <- config$bcDir
   }
   
-  return(paste(input.dir, path, site, sep="/"))
+  return(paste(input.dir, path, site, sep = "/"))
 }
 
 ReadObs <- function(site, var.name, file.id) {
   obs.dir <- GetDir(site, "obs")
   
-  suff <- paste0(var.name,"_", file.id)
-  obs <- read.csv(list.files(obs.dir, suff, full.names=T))
-  obs$val[obs$val==-99.9] <- NA
+  suff <- paste0(var.name, "_", file.id)
+  obs <- read.csv(list.files(obs.dir, suff, full.names = T))
+  obs$val[obs$val == -99.9] <- NA
   
   return(obs)
 }
@@ -28,37 +28,44 @@ ReadMod <- function(site, gcm, var.name, file.id) {
   mod.dir <- GetDir(site, "mod")
   file.name <- paste0(gcm, "_", var.name, "_", file.id, ".csv")
   
-  mod <- lapply(
-    setNames(
-      config$experiments, config$experiments),
-    function(exp){
-      read.csv(paste0(mod.dir, "/", exp, "/", file.name))
-    }) %>%
-    dplyr::bind_rows(.id="exp")
+  mod <- lapply(setNames(config$experiments, config$experiments),
+                function(exp) {
+                  read.csv(paste0(mod.dir, "/", exp, "/", file.name))
+                }) %>%
+    dplyr::bind_rows(.id = "exp")
   
   return(mod)
 }
 
 WriteBC <- function(out.dat, site, gcm, var.name, file.id) {
   bc.dir <- GetDir(site, "bc")
-  if(!dir.exists(bc.dir)) dir.create(bc.dir, recursive=T)
+  if (!dir.exists(bc.dir))
+    dir.create(bc.dir, recursive = T)
   
   file.name <- paste0(gcm, "_", var.name, "_", file.id, ".csv")
   print(file.name)
   
   for (e in config$experiments) {
-    
-    out.dir <- paste0(bc.dir,"/", e)
-    if(!file.exists(out.dir)) dir.create(out.dir)
+    out.dir <- paste0(bc.dir, "/", e)
+    if (!file.exists(out.dir))
+      dir.create(out.dir)
     
     print(paste0(out.dir, "/", file.name))
     
     bc <- out.dat %>%
       dplyr::filter(exp == e) %>%
       dplyr::ungroup() %>%
-      dplyr::select(year:day,val=round(bc,4))
+      dplyr::select(year:day, val = round(bc, 4))
     
-    write.table(bc, paste0(out.dir, "/", file.name), sep=",", row.names=F, col.names=T, quote=F, na="")
+    write.table(
+      bc,
+      paste0(out.dir, "/", file.name),
+      sep = ",",
+      row.names = F,
+      col.names = T,
+      quote = F,
+      na = ""
+    )
     
   }
 }
